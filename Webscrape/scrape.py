dbbfb3d6fa91de60cf531162146a3bfa818923a8
mynktwri <mayank@rockinfamily.com>
2018-10-29 20:18:14 -0700

    can get synonyms in dicts. now time to clean the dicts to only store the words themselves.

--- Webscrape/scrape.py
+++ Webscrape/scrape.py
@@ -2,23 +2,7 @@ from bs4 import BeautifulSoup as BS
 import requests
 import json
 from json import JSONDecoder
-def parse_syn(json):
-    # print (json["results"].items())
-    if isinstance(json, str):
-        print(json)
-    else:
-        for i in json:
-            print(i)
-            if (i =="synonyms"):
-                print("synonym found")
-                
-            elif (len(i) < 2):
-                continue
-            else:
-                if isinstance(i, str):
-                    parse_syn(json[i])
-                else:
-                    parse_syn(i)
+global synonyms
 
 app_id = '7248bd1c'
 app_key = 'c47f4d57c7c38204f8fd3846e6b725f9'
@@ -29,16 +13,30 @@ word_id = 'create'
 url = 'https://od-api.oxforddictionaries.com:443/api/v1/entries/' + language + \
  '/' + word_id.lower() + '/synonyms;antonyms'
 
-# r = requests.get(url, headers = {'app_id': app_id, 'app_key': app_key}).json()
+r = requests.get(url, headers = {'app_id': app_id, 'app_key': app_key})
 
-# print("code {}\n".format(r.status_code))
+print("code {}\n".format(r.status_code))
 #print("text \n" + r.text)
 
-with open('data.json') as file:
-    r = json.load(file)
-results = r["results"]
+# with open('data.json') as file:
+#     r = json.load(file)
+senses = r.json()["results"][0]["lexicalEntries"][0]["entries"][0]["senses"]
+
+for i in senses:
+    for j in i:
+        if (j == "synonyms"):
+            print(i[j])
+        elif(j == "subsenses"):
+            for k in i[j]:
+                print(k["synonyms"])
+          # [0]["senses"]["synonyms"])
+# results = r["results"]
 # print("json \n" + json.dumps(r))
-parse_syn(results)
+# senseslist = parse_syn(results)
+# for i in senseslist:
+#     for j in i:
+#         print(j)
+
 # print(results[0]["lexicalEntries"])
 
 # with open('data.json', 'w') as outfile:
