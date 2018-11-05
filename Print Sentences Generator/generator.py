127b7ee508621f2ea82164141ebf8a0721108126
Abdurrahman Al-Omari <abdurrahman.alomari@gmail.com>
2018-11-04 18:05:02 -0800

    script_out contains full string, print command, string to print.  all in a csv format

---
+++ Print Sentences Generator/generator.py
@@ -0,0 +1,13 @@
+printvar = open("print_var.txt").read().splitlines()
+words = open("common_words.txt").read().splitlines()
+output = open("script_out.txt", "w")
+
+SIZEOUT = len(printvar)
+SIZEIN = len(words)
+
+for i in range (0, SIZEOUT):
+    for j in range (0, SIZEIN):
+        line = printvar[i] + " " + words[j] + "," + printvar[i] + "," + words[j] + "\n"
+        output.write(line)
+
+output.close()        
