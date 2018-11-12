26dccff937d28dfd7f570be5685db462c9f7b8d4
mynktwri <mayank@rockinfamily.com>
2018-11-11 22:28:19 -0800

    script to clean database is db_clean.py clean_terms.csv has the terms now sorted alphabetically and without duplicates.

---
+++ Webscrape/db_clean.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+data_df = pd.read_csv("terms_synonyms.csv", )
+data_df = data_df.drop(data_df.columns[:1], axis=1)
+data_df["word"] = data_df["word"].str.lower()
+data_df = data_df.drop_duplicates(["word"])
+print(data_df["word"])
+# data_df = data_df.drop(data_df.columns[0], axis=1)
+data_df = data_df.sort_values(by="word", axis=0)
+data_df.reset_index(drop=True, inplace=True)
+data_df.to_csv("clean_terms.csv")
