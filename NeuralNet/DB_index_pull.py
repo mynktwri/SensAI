34139e56a38806a5db5b2d0455bcb20bb2cb4a9c
mynktwri <mayank@rockinfamily.com>
2018-11-16 01:08:56 -0800

    can take words from a sentence and look them up in our dict. It continues to do so until every word in the dataset has been looked up or added into the database. In adding, TODO: ask the user to categorize the new words so the AI can actively learn.

--- NeuralNet/DB_index_pull.py
+++ NeuralNet/DB_index_pull.py
@@ -21,25 +21,24 @@ def db_get(word, df):
         return -1
 
 
-def parse_input(sentence, df):
+def parse_input(sentences, df):
     # TODO: sentence into NLP goes here
 
     # TODO: parse through our database and add that to tensor
-    newword = False
-    while not newword:
-        word_db_id = []
-        df = db_clean.db_clean(df)
-        newword = True
-        for i in range(len(sentence)):
-            temp = db_get(sentence[i].lower(), df)
-            if temp == -1:
-                df = learn.new_word(sentence[i].lower(), df)
-                newword = False
-            else:
-                word_db_id.append(temp)
-    print(word_db_id)
-    return df
-    #
+    for sentence in sentences:
+        newword = False
+        while not newword:
+            word_db_id = []
+            df = db_clean.db_clean(df)
+            newword = True
+            for i in range(len(sentence)):
+                temp = db_get(sentence[i].lower(), df)
+                if temp == -1:
+                    df = learn.new_word(sentence[i].lower(), df)
+                    newword = False
+                else:
+                    word_db_id.append(temp)
+        return df, word_db_id
 
 
 # training data
@@ -59,4 +58,6 @@ train_string = [["set", "x", "equal", "to", "5"], ["output", "x"], ["loop", "thr
 train_data = [[0, 9999, 5, 9999, 9999], [46, 9999], [17, 20, 30, 9999, 9999, 9999], [45, 1287], [0, 403, 9999, 9999]]
 test_train_data = []
 for i in train_string:
-    data_df = parse_input(i, data_df)
+    data_df, sentence = parse_input(i, data_df)
+    test_train_data.append(sentence)
+    print(sentence, " | ", i)
