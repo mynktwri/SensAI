d4a66010bd87fae96f69a3bd417817f276e451ab
aagusuab <younjae12@gmail.com>
2018-11-25 22:30:34 -0800

    Edited new version of Process.py and Test.py.

--- NLP/Process.py
+++ NLP/Process.py
@@ -5,7 +5,42 @@ from nltk.tokenize import word_tokenize
 def getTag(text):
     text = word_tokenize(text)
     pos_tags = nltk.pos_tag(text)
-    return pos_tags
+    verbs = ["MD", "TO", "VB", "VBD", "VBG", "VBN", "VBP", "VBZ"]
+    nouns = ["PRP", "PRP$", "POS", "NN", "NNS", "FW", "UH", "NNP", "NNPS"]
+    adj = ["JJ", "JJR", "JJS", "PDT"]
+    Adv = ["RB", "RBR", "RBS", "EX", "WDT", "WP", "WP$", "WRB"]
+    prep = ["IN"]
+    number = ["CD"]
+    symbol = ["SYMBOL"]
+    article = ["DT"]
+    participle = ["RP"]
+    quantifier = ["LS"]
+    pos_map = {"adjective": ["JJ", "JJR", "JJS", "PDT"],
+               "adverb": ["RB", "RBR", "RBS", "EX", "WDT", "WP", "WP$", "WRB"],
+               "common_noun": ["PRP", "PRP$", "POS", "NN", "NNS", "FW", "UH"],
+               "proper_noun": ["NNP", "NNPS"],
+               "verb": ["MD", "TO", "VB", "VBD", "VBG", "VBN", "VBP", "VBZ"],
+               "symbol": ["SYM"
+                          "BOL"],
+               "article": ["DT", ],
+               "coordinating_conj": ["CC"],
+               "demonstrative_pronoun": [],
+               "numeral": ["CD", ],
+               "participle": ["RP"],
+               "indefinite_pronoun": [],
+               "preposition": ["IN"],
+               "quantifier": ["LS"],
+               "subordinating_conj": [],
+               "punctuation": ["."]}
+    simple_tags = []
+    for p in pos_tags:
+        for k, v in pos_map.items():
+            if p[1] in v:
+                simple_tags.append(k)
+            else:
+                pass
+
+    return simple_tags
 
 
 
