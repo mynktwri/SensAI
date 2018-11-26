9fb21b4d7e1e7425c2588ee566e100da2f6e0411
aagusuab <younjae12@gmail.com>
2018-11-25 20:57:45 -0800

    Edited new version of Process.py and Test.py.

--- NLP/Process.py
+++ NLP/Process.py
@@ -1,14 +1,29 @@
-import stanford_tools as st
-import dsynt_generator as dg
-import element_utils as eu
-import config as config
-import subprocess
+import nltk
+from nltk.tokenize import word_tokenize
 
-def NLP(text):
-    st.init()
-    texttree = st.parse_sentences([text], "test")
-    texttree = st.putIntoDepTree(texttree)
-    text_dsynt = dg.convertToDsynts(texttree)
-    print(type(text_dsynt))
-    eu.writeTree(text_dsynt, "output.xml")
-    return text_dsynt
+
+def getTag(text):
+    text = word_tokenize(text)
+    pos_tags = nltk.pos_tag(text)
+    return pos_tags
+
+
+
+def getWord(text):
+    text = word_tokenize(text)
+    return text
+
+
+
+def getLength(text):
+    text = word_tokenize(text)
+    pos_tags = nltk.pos_tag(text)
+    return len(pos_tags)
+
+
+def numVerbs(text):
+    tokens = word_tokenize(text)
+    pos_tags = nltk.pos_tag(tokens)
+    verbs = ["MD", "TO", "VB", "VBD", "VBG", "VBN", "VBP", "VBZ"]
+    pos_list = [p for p in pos_tags if p[1] in verbs]
+    return len(pos_list)
