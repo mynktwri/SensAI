192127f4c9decc86313725c38594c501ccbd7721
mynktwri <mayank@rockinfamily.com>
2018-11-30 03:16:42 -0800

    fixed data collection. now the data is shuffled into train/test_data and split. validation data is split in the model

--- NeuralNet/evaluateNN.py
+++ NeuralNet/evaluateNN.py
@@ -5,7 +5,8 @@ from tensorflow import keras
 import DB_index_pull as db_pull
 import numpy as np
 import pandas as pd
-
+import random
+random.seed(7)
 
 
 def db_getlabel(input):
@@ -70,28 +71,36 @@ input_labels = pd.concat([variable_targets, print_targets, loop_targets], axis=0
 
 train_data = pd.DataFrame()
 train_labels = pd.Series()
-validation_data = pd.DataFrame()
-validation_labels = pd.Series()
+# validation_data = pd.DataFrame()
+# validation_labels = pd.Series()
 test_data = pd.DataFrame()
 test_labels = pd.Series()
 
+shuffled_data = pd.DataFrame.reset_index(pd.DataFrame.sample(pd.concat([input_data, input_labels],
+                                                                       axis=1, ignore_index=True),
+                                                             frac=1))
+train_labels = shuffled_data[20]
+train_data = shuffled_data.drop(columns=20)
+train_data, test_data = np.split(train_data, [int(.9*len(train_data))])
+train_labels, test_labels = np.split(train_labels, [int(.9*len(train_labels))])
 
-for i in range(1, len(input_data)):
-    if i % 7 == 0:
-        test_data = pd.concat([test_data, input_data[:i]], axis=0, ignore_index=True)
-        test_labels.append(pd.Series(input_labels[i]), ignore_index=True)
-        print(i/len(input_data))
-    if i % 10 == 0:
-        validation_data = pd.concat([validation_data, input_data[:i]], axis=0, ignore_index=True)
-        validation_labels.append(pd.Series(input_labels[i]), ignore_index=True)
-        print(i / len(input_data))
-    else:
-        train_data = pd.concat([train_data, input_data[:i]], axis=0, ignore_index=True)
-        train_labels.append(pd.Series(input_labels[i]), ignore_index=True)
+
+# for i in range(1, len(input_data)):
+#     if i % 7 == 0:
+#         test_data = pd.concat([test_data, input_data[:i]], axis=0, ignore_index=True)
+#         test_labels.append(pd.Series(input_labels[i]), ignore_index=True)
+#         print(i/len(input_data))
+#     if i % 10 == 0:
+#         validation_data = pd.concat([validation_data, input_data[:i]], axis=0, ignore_index=True)
+#         validation_labels.append(pd.Series(input_labels[i]), ignore_index=True)
+#         print(i / len(input_data))
+#     else:
+#         train_data = pd.concat([train_data, input_data[:i]], axis=0, ignore_index=True)
+#         train_labels.append(pd.Series(input_labels[i]), ignore_index=True)
 print(test_data.shape)
 print(test_labels.shape)
-print(validation_data.shape)
-print(validation_labels.shape)
+# print(validation_data.shape)
+# print(validation_labels.shape)
 print(train_data.shape)
 print(train_labels.shape)
 exit(0)
@@ -110,10 +119,10 @@ model.add(keras.layers.GlobalAveragePooling1D())
 # Hidden Layer
 model.add(keras.layers.Dense(16, activation=tf.nn.relu))
 # Output Layer: needs as many nodes as there are categories.
-model.add(keras.layers.Dense(1, activation=tf.nn.sigmoid))
+model.add(keras.layers.Dense(3, activation='softmax'))
 
 model.compile(optimizer=tf.train.AdamOptimizer(),
-              loss='binary_crossentropy', #TODO: change loss function to one that can do multi-class classification
+              loss='categorical_crossentropy',
               metrics=['accuracy']) # consider RMSE or MSE as metric for error
 
 #  Train the model
@@ -122,7 +131,8 @@ history = model.fit(x=train_data,
                     y=train_labels,
                     epochs=40,
                     batch_size=512,
-                    validation_data=(validation_data, validation_labels),
+                    validation_split = 0.1,
+                    # validation_data=(validation_data, validation_labels),
                     verbose=1)
 print(history)
 results = model.evaluate(test_data, test_labels)
