57deeaa3768263e6bce0238127733dbc41c38b3f
mynktwri <mayank@rockinfamily.com>
2018-11-30 00:27:24 -0800

    i think ive fixed the bug, however it is exceedingly slow with this method. going to now use a different method: pick random indices until 10% of data has been picked for testing and 10% for validation. These percentages can always be changed later.

--- NeuralNet/evaluateNN.py
+++ NeuralNet/evaluateNN.py
@@ -68,31 +68,37 @@ input_data = pd.concat([id_list, pos_list], axis=1, ignore_index=True)
 input_labels = pd.concat([variable_targets, print_targets, loop_targets], axis=0, ignore_index=True)
 
 
-train_data = []
-train_labels = []
-validation_data = []
-validation_labels = []
-test_data = []
-test_labels = []
+train_data = pd.DataFrame()
+train_labels = pd.Series()
+validation_data = pd.DataFrame()
+validation_labels = pd.Series()
+test_data = pd.DataFrame()
+test_labels = pd.Series()
 
-i = 0
-for row in input_data:
-    if (i % 7 == 0):
-        test_data = test_data + input_data[i]
-        test_labels = test_labels + input_labels[i]
-    if (i % 10 == 0):
-        validation_data = validation_data + [input_data[i]]
-        validation_labels = validation_labels + [input_labels[i]]
+
+for i in range(1, len(input_data)):
+    if i % 7 == 0:
+        test_data = pd.concat([test_data, input_data[:i]], axis=0, ignore_index=True)
+        test_labels.append(pd.Series(input_labels[i]), ignore_index=True)
+        print(i/len(input_data))
+    if i % 10 == 0:
+        validation_data = pd.concat([validation_data, input_data[:i]], axis=0, ignore_index=True)
+        validation_labels.append(pd.Series(input_labels[i]), ignore_index=True)
+        print(i / len(input_data))
     else:
-        train_data = train_data + [input_data[i]]
-        train_labels = train_labels + [input_labels[i]]
-    i = i + 1
-
+        train_data = pd.concat([train_data, input_data[:i]], axis=0, ignore_index=True)
+        train_labels.append(pd.Series(input_labels[i]), ignore_index=True)
+print(test_data.shape)
+print(test_labels.shape)
+print(validation_data.shape)
+print(validation_labels.shape)
+print(train_data.shape)
+print(train_labels.shape)
+exit(0)
 # Build the model
 
 # vocab_size is the size of our database at model initialization.
 # this ensures that all new words have already been added to the database.
-# TODO
 vocab_size = db_pull.db_len
 
 model = keras.Sequential()
@@ -107,7 +113,7 @@ model.add(keras.layers.Dense(16, activation=tf.nn.relu))
 model.add(keras.layers.Dense(1, activation=tf.nn.sigmoid))
 
 model.compile(optimizer=tf.train.AdamOptimizer(),
-              loss='binary_crossentropy', #change loss function to one that can do multi-class classification
+              loss='binary_crossentropy', #TODO: change loss function to one that can do multi-class classification
               metrics=['accuracy']) # consider RMSE or MSE as metric for error
 
 #  Train the model
