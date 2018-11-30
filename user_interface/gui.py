e813ec6b9a012515d78944e43790a3371769f3a8
Kalinda Fraser <klfraser@ucsc.edu>
2018-11-29 20:22:10 -0800

    created basic ui, _does not_ yet interface with NN

---
+++ user_interface/gui.py
@@ -0,0 +1,33 @@
+import tkinter
+#import neural-net
+from tkinter import *
+
+root = Tk()
+top = Frame(root)
+top.pack(side = TOP)
+L1 = Label(top, text="Enter a sentence to feed to neural net.")
+L1.pack( side = LEFT)
+E1 = Entry(top, bd =5)
+E1.pack(side = RIGHT)
+
+output = StringVar()
+
+def callNN():
+    output.set(E1.get())
+    L2.pack()
+    sentence = E1.get()
+    #prediction = enn.makePrediction(sentence)
+    #TODO: move to ENN
+    #convertedSentence = parse_input(sentence)
+    #model.predict(convertedSentence)
+    #for now, our "prediction" is the input sentence
+    prediction = sentence
+    output.set(prediction)
+bottom = Frame(root)
+bottom.pack(side = BOTTOM)
+B = Button(bottom, text ="Evaluate", command = callNN)
+B.pack(side = TOP)
+L2 = Label(bottom, textvariable=output, relief=RAISED)
+L2.pack(side = BOTTOM)
+
+root.mainloop()
